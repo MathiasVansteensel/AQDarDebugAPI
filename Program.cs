49674b0ef11c592d9cf84c8b6ae48102f86604db
Mathias Vansteensel <mathias.vansteensel@gmail.com>
2025-04-17 22:59:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace AQDarDebugAPI;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+        //app.UseHttpsRedirection();
+
+        app.Urls.Clear();
+        app.Urls.Add("http://*:6969/");
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Static")),
+            RequestPath = "/Static"
+        });
+
+        app.MapGet("/", () => Results.Content(File.ReadAllText("wwwroot/root.html"), "text/html"));
+
+        await app.RunAsync();
+    }
+
+}
